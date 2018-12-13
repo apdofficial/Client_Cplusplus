197d790c6906a719ba334b13c5ffac3b6ad7d23e
Andrej Pistek <andrej.pistek@outlook.com>
2018-12-13 09:14:57 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Cplusplus_client.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cplusplus_client.dir/main.cpp.o"
+  "Cplusplus_client.pdb"
+  "Cplusplus_client"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cplusplus_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
